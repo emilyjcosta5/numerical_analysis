df11494d9430f1a76b391b7da8ffe8142ee9d9f0
emily janet costa <ecost020@yavin.aul.fiu.edu>
2020-01-17 16:05:39 -0500

    Completed bisection method

---
+++ bisection_method.jl
@@ -0,0 +1,23 @@
+
+function bisection_method(f::Function, a::Number, b::Number,
+                   E::AbstractFloat=1e-5, N_0::Integer=1000)
+    fa = f(a)
+    p = 0
+    for n in N_0
+        p = a + (b-a)/2
+        fp = f(p)
+        if (fp == 0 || fp < E)
+            break
+        end
+        if (fa*fp > 0)
+            a = p
+            fa = fp
+        else b = p
+        end
+    end
+    return p 
+end
+
+bisection_method(p -> p^3-p-1,1,2)
+
+
